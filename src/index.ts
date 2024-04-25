5c57a41471b6f4494bc3dfd521f5909558024e13
Paula <paula.uxengineer@gmail.com>
2024-04-25 18:10:15 +0200

    update scripts

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@ import express from 'express';
 
 const app = express();
 
-// Server port
-const PORT = process.env.PORT || 3000; // port:3000 is for the server; it should listen on a different port than the database
+const PORT = process.env.PORT || 3000; 
 
 app.listen(PORT, () => {
   console.log(`Server listening at port ${PORT}`);
--- src/optical_store/connection_database.ts
+++ src/optical_store/connection_database.ts
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
-  const poolOptical = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: 3306 // same port to mysql server
-  });
+const poolOptical = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: 3306 // same port to mysql server
+});
 
 export default poolOptical
